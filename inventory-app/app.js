ec00593004212c6c6e4fd356969790e70e4a860e
Shriharsh <shriharsh.dev@gmail.com>
2023-10-30 17:09:12 +0530

    Added routes as middleware

--- inventory-app/app.js
+++ inventory-app/app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog')
 
+
 const app = express();
 
 //connect to database
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog',catalogRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
