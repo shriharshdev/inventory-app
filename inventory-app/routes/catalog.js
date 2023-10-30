23ee232428ecec7bd5aab97931bfc853044ac8bc
Shriharsh <shriharsh.dev@gmail.com>
2023-10-30 17:08:30 +0530

    Modified some routes and added new routes

--- inventory-app/routes/catalog.js
+++ inventory-app/routes/catalog.js
@@ -1,26 +1,34 @@
-const express = require('express')
-const router = express.Router
+const express = require("express")
+const router = express.Router()
 
-const item_controller = require("../controller/itemController")
 const category_controller = require("../controller/categoryController")
+const item_controller = require("../controller/itemController")
 
 router.get("/",category_controller.index)
+
 router.get("/category/create",category_controller.category_create_get)
-router.get("/category/create",category_controller.category_create_post)
+router.post("/category/create",category_controller.category_create_post)
+
 router.get("/category/:id/delete",category_controller.category_delete_get)
 router.get("/category/:id/delete",category_controller.category_delete_post)
+
 router.get("/category/:id/update",category_controller.category_update_get)
-router.get("/category/:id/update",category_controller.category_update_post)
+router.post("/category/:id/update",category_controller.category_update_post)
+
 router.get("/category/:id",category_controller.category_detail)
-router.get("/category/:id",category_controller.category_list)
+router.get("/categories",category_controller.category_list)
+
 
 router.get("/item/create",item_controller.item_create_get)
-router.get("/item/create",item_controller.item_create_post)
+router.post("/item/create",item_controller.item_create_post)
+
 router.get("/item/:id/delete",item_controller.item_delete_get)
 router.get("/item/:id/delete",item_controller.item_delete_post)
-router.get("/item/:id/update",item_controller.item_update_get)
-router.get("/item/:id/update",item_controller.item_update_post)
-router.get("/item/:id",item_controller.item_detail)
-router.get("/item/:id",item_controller.item_list)
 
-module.exports = router;
+router.get("/item/:id/update",item_controller.item_update_get)
+router.post("/item/:id/update",item_controller.item_update_post)
+
+router.get("/item/:id",item_controller.item_detail)
+router.get("/items",item_controller.item_list)
+
+module.exports = router
