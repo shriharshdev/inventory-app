5564737975081fa4515caaadf5b9215120348907
Shriharsh <shriharsh.dev@gmail.com>
2023-11-04 09:29:55 +0530

    Changed category to Array

--- inventory-app/models/items.js
+++ inventory-app/models/items.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const itemSchema = new Schema({
     name:{type:String, minLength:4,maxLength:20, required:true},
     description:{type:String, minLength:4,maxLength:200, required:true},
-    category:{type:Schema.Types.ObjectId, ref:"category", required:true},
+    category:[{type:Schema.Types.ObjectId, ref:"category", required:true}],
     price:{type:Number, min:1,  required:true},
     stock:{type:Number, required:true},
     
