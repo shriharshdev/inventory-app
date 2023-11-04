fcb82f7097bcdd4975c1e561a1d0f1dc38d571a2
Shriharsh <shriharsh.dev@gmail.com>
2023-11-04 09:31:26 +0530

    Changed the category and item delete routes

--- inventory-app/routes/catalog.js
+++ inventory-app/routes/catalog.js
@@ -10,7 +10,7 @@ router.get("/category/create",category_controller.category_create_get)
 router.post("/category/create",category_controller.category_create_post)
 
 router.get("/category/:id/delete",category_controller.category_delete_get)
-router.get("/category/:id/delete",category_controller.category_delete_post)
+router.post("/category/:id/delete",category_controller.category_delete_post)
 
 router.get("/category/:id/update",category_controller.category_update_get)
 router.post("/category/:id/update",category_controller.category_update_post)
@@ -23,7 +23,7 @@ router.get("/item/create",item_controller.item_create_get)
 router.post("/item/create",item_controller.item_create_post)
 
 router.get("/item/:id/delete",item_controller.item_delete_get)
-router.get("/item/:id/delete",item_controller.item_delete_post)
+router.post("/item/:id/delete",item_controller.item_delete_post)
 
 router.get("/item/:id/update",item_controller.item_update_get)
 router.post("/item/:id/update",item_controller.item_update_post)
